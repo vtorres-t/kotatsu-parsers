78c564f20018bb41c59411b812b008165b3784be
Vicente <vicente@per>
2025-10-13 18:21:30 +0200

    Broken sources

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/TaurusManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/TaurusManga.kt
@@ -4,7 +4,9 @@ import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
+import org.koitharu.kotatsu.parsers.Broken
 
+@Broken
 @MangaSourceParser("TAURUSMANGA", "TaurusManga", "es")
 internal class TaurusManga(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.TAURUSMANGA, "taurus.topmanhuas.org") {
